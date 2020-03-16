2d92c5867b5c3089d32944e20d86c77220992f4b
Matthew VH <vanheckematt@gmail.com>
2020-03-16 14:09:42 -0500

    Added SuperherosController, Superhero model, and Superheros folder (in Views). Also added Superheros property to ApplicationDbContext and properties for Name, AlterEgo, PrimarySuperheroAbility, and SecondarySuperheroAbility to Superhero model. Next goal: Start creating views.

--- SuperheroManager/SuperheroManager/Data/ApplicationDbContext.cs
+++ SuperheroManager/SuperheroManager/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SuperheroManager.Models;
 
 namespace SuperheroManager.Data
 {
@@ -12,5 +13,6 @@ namespace SuperheroManager.Data
             : base(options)
         {
         }
+        public DbSet<Superhero> Superheros { get; set; }
     }
 }
---
+++ SuperheroManager/SuperheroManager/Models/Superhero.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperheroManager.Models
+{
+    public class Superhero
+    {
+        public string Name { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimarySuperheroAbility { get; set; }
+        public string SecondarySuperheroAbility { get; set; }
+    }
+}
